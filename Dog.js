155a85b10821299939b91b72f6f900fe7951612b
Saul Almanzar <almanzarsaul@icloud.com>
2023-04-15 11:31:38 -0400

    Early JS structure

--- Dog.js
+++ Dog.js
@@ -1,1 +1,19 @@
 // Create the Dog class here
+
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+
+  getDogHtml() {
+    const { name, avatar, age, bio } = this;
+
+    return `
+        <img src="${avatar}" alt="${name}'s avatar." />
+        <h2 class="dog-details">${name}, ${age}</h2>
+        <p class="dog-description">${bio}</p>
+    `;
+  }
+}
+
+export default Dog;
--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 import dogs from "./data.js";
+import Dog from "./Dog.js";
 
 let remainingDogs = dogs;
 
-console.log(remainingDogs);
+function render() {
+  if (remainingDogs.length > 0) {
+    document.getElementById("dog-info").innerHTML = dog.getDogHtml();
+  } else {
+    document.getElementById("main").innerHTML = `
+    <h1>You're all caught up.</h1>
+    `;
+  }
+}
+
+let dog = new Dog(remainingDogs[0]);
+
+render();
+
+document.getElementById("dislike-btn").addEventListener("click", () => {
+  remainingDogs.shift();
+  dog = new Dog(remainingDogs[0]);
+  render();
+});
