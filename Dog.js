e08d3b138af732c129359f97ea7eae4e9ba5781e
Saul Almanzar <almanzarsaul@icloud.com>
2023-04-17 10:41:12 -0400

    Finish MVP

--- Dog.js
+++ Dog.js
@@ -5,10 +5,27 @@ class Dog {
     Object.assign(this, data);
   }
 
+  like() {
+    this.hasBeenLiked = true;
+  }
+
+  dislike() {
+    this.hasBeenSwiped = true;
+  }
+
+  getDogBadgeHtml() {
+    if (this.hasBeenLiked) {
+      return '<img class="dog-badge" src="images/badge-like.png" />';
+    } else if (this.hasBeenSwiped) {
+      return '<img class="dog-badge" src="images/badge-nope.png" />';
+    }
+  }
+
   getDogHtml() {
-    const { name, avatar, age, bio } = this;
+    const { name, avatar, age, bio, hasBeenLiked, hasBeenSwiped } = this;
 
     return `
+        ${hasBeenLiked || hasBeenSwiped ? this.getDogBadgeHtml() : ""}
         <img class="dog-avatar" src="${avatar}" alt="${name}'s avatar." />
         <div class="dog-details">
           <h2 class="dog-name-age">${name}, ${age}</h2>
--- index.js
+++ index.js
@@ -2,6 +2,34 @@ import dogs from "./data.js";
 import Dog from "./Dog.js";
 
 let remainingDogs = dogs;
+let likedDogs = [];
+let dislikedDogs = [];
+
+let dog = new Dog(remainingDogs[0]);
+
+render();
+
+document.getElementById("dislike-btn").addEventListener("click", (e) => {
+  dog.dislike();
+  dislikedDogs.push(dog);
+  render();
+  remainingDogs.shift();
+  dog = new Dog(remainingDogs[0]);
+  setTimeout(render, 1500);
+});
+
+document.getElementById("like-btn").addEventListener("click", (e) => {
+  dog.like();
+  likedDogs.push(dog);
+  render();
+  remainingDogs.shift();
+  dog = new Dog(remainingDogs[0]);
+  setTimeout(render, 1500);
+});
+
+document.getElementById("home-icon").addEventListener("click", (e) => {
+  location.reload();
+});
 
 function render() {
   if (remainingDogs.length > 0) {
@@ -10,15 +38,7 @@ function render() {
     document.getElementById("main").innerHTML = `
     <h1>You're all caught up.</h1>
     `;
+    console.log(likedDogs);
+    console.log(dislikedDogs);
   }
 }
-
-let dog = new Dog(remainingDogs[0]);
-
-render();
-
-document.getElementById("dislike-btn").addEventListener("click", (e) => {
-  remainingDogs.shift();
-  dog = new Dog(remainingDogs[0]);
-  render();
-});
